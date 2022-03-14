7a271bdbed03fa4d3f683d8bf768cd919521402a
ajinkyalandge <ajinkyalandge@gmail.com>
2022-03-14 13:59:05 +0530

    Initial Commit

---
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CommandsController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<string>> Get()
+    {
+        return new string[] {"this", "is", "hard", "coded"};
+    }
+}
